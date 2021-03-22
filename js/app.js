59b15ffbaa11ddcf936bef94731566cfae6316cc
Burhan Yusuf <burhanyusuf@outlook.com>
2021-03-22 18:11:45 +0700

    Update

--- js/app.js
+++ js/app.js
@@ -356,4 +356,55 @@ $('#nav').click(function(){
 });
 $('.close').click(function(){
     $('.overlay-navigation').removeClass('navigation-show')
+});
+
+
+(function() {
+ 
+    window.inputNumber = function(el) {
+  
+      var min = el.attr('min') || false;
+      var max = el.attr('max') || false;
+  
+      var els = {};
+  
+      els.dec = el.prev();
+      els.inc = el.next();
+  
+      el.each(function() {
+        init($(this));
+      });
+  
+      function init(el) {
+  
+        els.dec.on('click', decrement);
+        els.inc.on('click', increment);
+  
+        function decrement() {
+          var value = el[0].value;
+          value--;
+          if(!min || value >= min) {
+            el[0].value = value;
+          }
+        }
+  
+        function increment() {
+          var value = el[0].value;
+          value++;
+          if(!max || value <= max) {
+            el[0].value = value++;
+          }
+        }
+      }
+    }
+  })();
+  
+  inputNumber($('.input-number'));
+
+
+$("#cart").click(function(){
+    $(".cart").css("right","0px");
+})
+$(".close-cart").click(function(){
+    $(".cart").css("right","-441px");
 })
